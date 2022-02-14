e1f3ddd6fb7a743fa5dc6b27dba22bdd5ec3c7b7
yusun <yousun2286@naver.com>
2022-02-14 16:34:21 +0900

    models

--- customerprj/customer/models.py
+++ customerprj/customer/models.py
@@ -3,12 +3,10 @@ from django.utils import timezone
 
 
 class Customer(models.Model):
-    name = models.CharField(max_length=200)
+    name = models.CharField(max_length=100)
     birthdate = models.DateField()
-    email = models.TextField()
-    gender = models.BooleanField()
-    created_date = models.DateTimeField(default=timezone.now)
-    published_date = models.DateTimeField(blank=True, null=True)
+    email = models.EmailField()
+    gender = models.BooleanField(default=True)
 
     def __str__(self):
-        return self.name
+        return self.name + '(' + str(self.id) + ')'
